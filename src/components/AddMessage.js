53617d5bbb34cc30c4037f40ae06976730b8fc5b
stasya0903 <stasya0903@mail.ru>
2020-08-15 18:41:26 -0500

    added focus to the input

--- src/components/AddMessage.js
+++ src/components/AddMessage.js
@@ -1,30 +1,39 @@
 import React, {Component} from 'react';
 
-export default class AddMessage extends Component{
+export default class AddMessage extends Component {
     constructor(props) {
         super(props);
+
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(e){
+    handleChange(e) {
         this.props.onChange(e.target.value)
     }
-    handleSubmit(e){
 
+    handleSubmit(e) {
         this.props.onSubmit();
         e.preventDefault();
-
     }
 
+    handleKeyUp(event) {
+        if (event.keyCode === 13) this.props.onSubmit();
+    }
+
+
     render() {
-        let newMessage =  this.props.newMessage;
+        let newMessage = this.props.newMessage;
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <input type="text"
                            value={newMessage}
-                           onChange={this.handleChange}/>
+                           onChange={this.handleChange}
+                           onKeyUp={event => {
+                               this.handleKeyUp(event)
+                           }}
+                           />
                     <input type="submit"/>
                 </form>
             </div>
